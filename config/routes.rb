3959bfe4ff4104806467a6535b151707e87014c6
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-09 17:24:03 +0100

    fixes on brand show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/blog' => redirect("https://bestyerbamate.co/blog/", status: 301)
-  resources :brands do
-    resources :yerbas
-  end
+  resources :brands
+  resources :yerbas
 end
