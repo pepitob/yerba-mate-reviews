d01ac47fa4e9658036604ed1ef708d32c019405a
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-11 18:59:38 +0100

    added show more/less and JS

--- app/javascript/controllers/show_description_controller.js
+++ app/javascript/controllers/show_description_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-description"
 export default class extends Controller {
-  static targets = ["text"]
+  static targets = ["text", "button"]
 
   connect() {
     console.log("controller connected")
@@ -10,5 +10,10 @@ export default class extends Controller {
   show(event) {
     console.log(event);
     this.textTarget.classList.toggle('expanded');
+    if (this.textTarget.classList.contains('expanded')) {
+      this.buttonTarget.innerText = 'Show Less';
+    } else {
+      this.buttonTarget.innerText = 'Show More';
+    }
   }
 }
