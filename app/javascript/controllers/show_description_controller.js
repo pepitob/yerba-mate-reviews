d18c42e648c1fac3a48ae1fd9514c9233944ed50
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-11 19:36:09 +0100

    toggle button working

--- app/javascript/controllers/show_description_controller.js
+++ app/javascript/controllers/show_description_controller.js
@@ -12,8 +12,10 @@ export default class extends Controller {
     this.textTarget.classList.toggle('expanded');
     if (this.textTarget.classList.contains('expanded')) {
       this.buttonTarget.innerText = 'Show Less';
+      this.buttonTarget.classList.toggle('expanded');
     } else {
       this.buttonTarget.innerText = 'Show More';
+      this.buttonTarget.classList.toggle('expanded');
     }
   }
 }
