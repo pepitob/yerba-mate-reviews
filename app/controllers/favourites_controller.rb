db447708626aa564a708137695d5c6b8f3cb309f
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-19 12:38:00 +0100

    added create action to favourite button

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,7 @@
 class FavouritesController < ApplicationController
+  def create
+    @yerba = Yerba.find(params[:yerba_id])
+    @favourite = Favourite.create(yerba: @yerba, user: current_user)
+    redirect_to yerba_path(@yerba)
+  end
 end
