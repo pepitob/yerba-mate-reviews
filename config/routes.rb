56cb21e9a66b70bb39722f52deaad1cbdea60668
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-22 18:21:24 +0100

    added user profile controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/blog' => redirect("https://bestyerbamate.co/blog/", status: 301)
+  resources :users, only: [:show]
   resources :brands
   resources :yerbas
   resources :favourites, only: [:create, :destroy]
